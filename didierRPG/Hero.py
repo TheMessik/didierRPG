c52039a0f1e15e43398dfe51c22f7fbe31add79b
Jozko <jozefj44@gmail.com>
2020-06-07 12:54:07 +0200

    Item class created to represent items, wearable or otherwise

--- didierRPG/Hero.py
+++ didierRPG/Hero.py
@@ -10,6 +10,18 @@ class Hero:
     agility = 0
     intelligence = 0
 
+    # inventory stuff
+    # hero has initially no gear
+    helmet = ""
+    chest_plate = ""
+    pants = ""
+    left_hand = ""
+    right_hand = ""
+    spell = ""
+
+    inventory = []
+
+
     # starting level
     level = 1
 
@@ -19,38 +31,56 @@ class Hero:
     # jobs to increase hero's level and abilities
     # TODO: balance
     def dive(self):
+        """chance for a dinks drop and a slight xp increase for Constitution"""
         print("Hero's diving")
 
     def fit(self):
+        """xp increase for Constitution"""
         print("Hero's fitting")
 
     def woodcutting(self):
+        """chance for a dinks drop and slight xp increase for Strength"""
         print("Hero's cutting wood")
 
     def weights(self):
+        """xp increase for Strength"""
         print("Hero's lifting like a crazyman")
 
     def archery(self):
+        """chance for a dinks drop and slight xp increase for Agility"""
         print("Hero's got an arrow to the knee")
 
     def parkour(self):
+        """xp increase for Agility"""
         print("Hero broke his ankle while doing parkour Office style")
 
     def archeology(self):
+        """chance for a dinks drop and slight xp increase for Intelligence"""
         print("Hero found out where he came from")
 
     def read(self):
+        """xp increase for Intelligence"""
         print("Writing is for sissies")
 
     def monsterhunt(self):
+        """dinks drop and xp increase, based on the stats"""
         print("Monster dead, I think")
 
     def dungeon(self):
+        """basically monsterhunt, but harder, with higher rewards"""
         print("Hero dead")
 
-    def coloseum(self, other):
+    def coloseum(self, other, wager):
+        """ challenge other hero, with a wager"""
         assert isinstance(other, Hero), "You either fight a hero, or you don't fight at all"
 
+    # inventory
+
+
+
+
+
+    # hero creation and loading
     def new_hero(self, name):
         hero = Hero(name)
         return hero
@@ -72,10 +102,10 @@ class Hero:
         with open(f"didierRPG/hero_data/{name}", "r") as file:
             data = json.load(file)
             self.name = data["name"]
-            self.agility = data["agility"]
-            self.strength = data["strength"]
-            self.intelligence = data["intelligence"]
             self.constitution = data["constitution"]
+            self.strength = data["strength"]
+            self.agility = data["agility"]
+            self.intelligence = data["intelligence"]
             self.level = data["level"]
 
         return self
---
+++ didierRPG/Item.py
@@ -0,0 +1,13 @@
+class Item:
+    def __init__(self, name: str, requirements: dict, type_of_item: str):
+        # sanity checks
+        assert isinstance(name, str), "name must be a string. We're not Nazis, we use strings for names"
+        assert isinstance(requirements, dict), "requirements must be a dict. No requirements? So a baby can wear it?"
+        assert isinstance(type_of_item, str), "type_of_item must be string. Can't use a number, can I?"
+
+        assert type_of_item in ("helmet", "chestplate", "pants", "hand", "spell", "consumable"), \
+            "unrecognized type of item"
+
+        self.name = name
+        self.requirements = requirements
+        self.type_of_item = type_of_item
